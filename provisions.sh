7068ab70e8eee980fe514e5598924c72b0323f93
Panihe <57388777+Panihe@users.noreply.github.com>
2020-11-09 11:32:09 +0100

    Subiendo de nuevo los cambios

--- provisions.sh
+++ provisions.sh
@@ -1,8 +1,12 @@
 apt-get -y update
 apt-get -y install nginx
 
-cp -R /usr/share/nginx/www /vagrant
-ln  -s /vagrant/www /usr/share/nginx/www
+# cp -R /usr/share/nginx/www /vagrant
+# ln  -s /vagrant/www /usr/share/nginx/www
+
+cp -R /vagrant/sites-enabled/ /etc/nginx/sites-enabled/
 
 service nginx start
+
+
 
