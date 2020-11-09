04c7be34a2b045374aef03130146f2bac2689051
Panihe <57388777+Panihe@users.noreply.github.com>
2020-11-09 12:09:48 +0100

    Borro lineas del provision, modifico ruta

--- provisions.sh
+++ provisions.sh
@@ -1,10 +1,11 @@
 apt-get -y update
 apt-get -y install nginx
+#rm -rf /etc/nginx/sites-enabled
 
 # cp -R /usr/share/nginx/www /vagrant
 # ln  -s /vagrant/www /usr/share/nginx/www
 
-cp -R /vagrant/sites-enabled/ /etc/nginx/sites-enabled/
+#cp -R /vagrant/sites-enabled /etc/nginx/
 
 service nginx start
 
